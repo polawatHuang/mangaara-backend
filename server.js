5cf47584043249098943b07a4d29c359342f831d
Polawat Huang <polawathuang@gmail.com>
2025-04-19 13:35:24 +0700

    add test route to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mangaRoutes = require('./routes/manga');
+const testRoutes = require('./routes/test');
 
 const app = express();
 const PORT = 3001;
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/mangas', mangaRoutes);
+app.use('/api/test', testRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
