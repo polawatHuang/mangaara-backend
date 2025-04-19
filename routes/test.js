4cb25cb67507bc99f62c7612f470f513dbb12fac
Polawat Huang <polawathuang@gmail.com>
2025-04-19 13:33:22 +0700

    Add test.js

---
+++ routes/test.js
@@ -0,0 +1,18 @@
+// routes/test.js
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+// Create
+router.post("/", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT NOW() AS time");
+    return Response.json({ server_time: rows[0].time });
+  } catch (err) {
+    console.error("[DB ERROR]", err);
+    return new Response(JSON.stringify({ error: err.message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+});
